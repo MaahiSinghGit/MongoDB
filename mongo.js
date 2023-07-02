ddb197fd61ff3ef1481ff765a3126b68ab410e26
MaahisinghGit <maahisingh07m@gmail.com>
2023-07-02 21:19:29 +0530

    MongoBC Connection

--- mongo.js
+++ mongo.js
@@ -1,4 +1,22 @@
+const MongoClient=require('mongodb').MongoClient;
+const url='mongodb+srv://Manoj-Bisht:<password>@cluster0.a1bfmfa.mongodb.net/products_test?retryWrites=true&w=majority';
 const  createProduct=async(req,res,next)=>{
+    const newProduct={
+        name:req.body.name,
+        price:req.body.price
+    }
+
+    const client =new MongoClient(url);
+
+    try{
+        await client.connect();
+        const db=client.db();
+        const result=db.collection('products').insertOne(newProduct);
+
+    }catch(error){
+        return res.json({message:'unable to store data'});
+    };
+    client.close();
     
 };
 
